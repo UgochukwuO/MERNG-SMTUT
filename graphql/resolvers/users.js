c2d1d8447dabbcccad8cda27e537ba73cbf948ca
Ugochukwu Onwuzurike <ugo.onwuzurike@yahoo.com>
2021-04-24 04:29:05 -0500

    Update

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,100 +1,129 @@
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-const { UserInputError } = require('apollo-server')
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { UserInputError } = require("apollo-server");
 
-const { validateRegisterInput, validateLoginInput } = require('../../util/validators')
-const { SECRET_KEY } = require('../../config')
-const User = require('../../models/User')
+const {
+  validateRegisterInput,
+  validateLoginInput,
+} = require("../../util/validators");
+const { SECRET_KEY } = require("../../config");
+const User = require("../../models/User");
 
+// Create Temp Token For User
 const generateToken = (user) => {
-    
-    return jwt.sign({
-        id: user.id,
-        email: user.email,
-        username: user.username
-    }, SECRET_KEY, { expiresIn: '1h'})
-}
+  return jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    },
+    SECRET_KEY,
+    { expiresIn: "1h" }
+  );
+};
 
 module.exports = {
-    Mutation: {
-        async login(_, { username, password }) {
-            const { errors, valid } = validateLoginInput(username, password)
+  Mutation: {
+    // Mutation to Login User
+    async login(_, { username, password }) {
+      //('../../util/validators')
+      const { errors, valid } = validateLoginInput(username, password);
 
-            if (!valid) {
-                throw new UserInputError('Errors', { errors })
-            }
+      // Check to see if user input follows our validation rules in ('../../util/validators')
+      if (!valid) {
+        throw new UserInputError("Errors", { errors }); // import from apollo-server node module
+      }
 
-            const user = await User.findOne({ username })
+      // Query to find an argument, username
+      const user = await User.findOne({ username });
 
-            if (!user) {
-                errors.general = 'User not found'
-                throw new UserInputError('User not found', { errors })
-            }
+      // Check if the user exist in the DB
+      if (!user) {
+        errors.general = "User not found"; // if user not found then errors should output
+        throw new UserInputError("User not found", { errors });
+      }
 
-            const match = await bcrypt.compare(password, user.password)
-            if (!match) {
-                errors.general = 'Wrong credentials'
-                throw new UserInputError('Wrong credentials', { errors })
-            }
+      // compare user input password with password associated with the user
+      const match = await bcrypt.compare(password, user.password);
 
-            const token =  generateToken(user)
-            return {
-                ...user._doc,
-                id: user._id,
-                token
-            }
-        },
+      // Check if password in DB matched user inputted password
+      if (!match) {
+        errors.general = "Password Incorrect"; // if input password does not match DB password
+        throw new UserInputError("Password Incorrect", { errors });
+      }
 
-        async register(_, 
-            { 
-                registerInput: { username, email, password, confirmPassword}
-            },
-            ) {
-
-            // Validate user data
-            const { valid, errors } = validateRegisterInput (username, email, password, confirmPassword)
-            if (!valid) {
-                throw new UserInputError('Errors', { errors })
-            }
-
-            // Make sure user does not already exist
-            const user = await User.findOne({ username })
-            if (user) {
-                throw new UserInputError('Username is taken', {
-                    errors: {
-                        username: 'This username is taken'
-                    }
-                })
-            }
-            // Hash pass before enter in DB and create an auth token
-            password = await bcrypt.hash(password, 12)
-
-            const newUser = new User ({
-                email,
-                username,
-                password,
-                createdAt: new Date().toISOString()
-            })
-
-            const res = await newUser.save()
-
-            const token =  generateToken(res)
-
-            return {
-                ...res._doc,
-                id: res._id,
-                token
-            }
-        }
+      // create temp user token if everything passes
+      const token = generateToken(user);
+      // If everything passes, return the user and token.
+      return {
+        ...user._doc,
+        id: user._id,
+        token,
+      };
     },
-    Query: {
-        async getUsers() {
-            try {
-                const users = await User.find()
-                return users
-            } catch(err) {
-                throw new Error(err)
-            }
-        }
-    }
-}
+
+    // Mutation to Register User
+    async register(
+      _,
+      { registerInput: { username, email, password, confirmPassword } }
+    ) {
+      // Validate user data
+      const { valid, errors } = validateRegisterInput(
+        username,
+        email,
+        password,
+        confirmPassword
+      ); //
+      if (!valid) {
+        throw new UserInputError("Errors", { errors });
+      }
+
+      // Make sure user does not already exist
+      const user = await User.findOne({ username });
+
+      // Check if the registered username is already in the DB
+      if (user) {
+        throw new UserInputError("Username is taken", {
+          errors: {
+            username: "This username is taken",
+          },
+        });
+      }
+      // Hash pass before enter in DB and create an auth token
+      password = await bcrypt.hash(password, 12);
+
+      // Create a new user
+      const newUser = new User({
+        email,
+        username,
+        password,
+        createdAt: new Date().toISOString(),
+      });
+
+      //Save new user to the variable 'res' short for result
+      const res = await newUser.save();
+
+      // Generate a temp token using the 'res' or new user data. id, email, username, secret_key
+      const token = generateToken(res);
+
+      // If everything passes, return the user and token.
+      return {
+        ...res._doc, // Spread out everything in the 'res'/newUser object.
+        id: res._id,
+        token,
+      };
+    },
+  },
+
+  Query: {
+    // Query to retrieve a list of all users
+    async getUsers() {
+      try {
+        const users = await User.find(); // Gets a list of all Users
+        return users;
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+  },
+};
--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const { ApolloServer } = require('apollo-server')
-const mongoose = require('mongoose') 
+const { ApolloServer } = require("apollo-server");
+const mongoose = require("mongoose");
 
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
+// Key to connect to database
+const { MONGODB } = require("./config.js");
 
-const typeDefs = require('./graphql/typeDefs')
-const resolvers = require('./graphql/resolvers')
-const { MONGODB } = require('./config.js')
+// Config the server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
-const server = new ApolloServer ({
-    typeDefs,
-    resolvers
-})
-
-
-// TODO: Connect Mongo DB 
-mongoose.connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true  })
-    .then(() => {
-        console.log(`MongoDB Connected`)
-        return server.listen({ port: 5000})
-    })
-    .then((res) => {
-        console.log(`Server Up and Running at ${res.url}`)
-    })
-
+// TODO: Connect Mongo DB
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log(`MongoDB Connected`);
+    return server.listen({ port: 5000 });
+  })
+  .then((res) => {
+    console.log(`Server Up and Running at ${res.url}`);
+  });
